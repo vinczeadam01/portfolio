f57ef5792b13c60f53ad04aa5e0cbdca6903070d
Vincze Adam <adam.vincze01@gmail.com>
2023-01-01 23:19:28 +0100

    add emailjs to contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,16 +1,45 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import "./contact.css"
 import {TbMessageDots} from "react-icons/tb"
 import {AiOutlineMail} from "react-icons/ai"
 import {BiMobileAlt} from "react-icons/bi"
+import emailjs from "@emailjs/browser"
 
 const Contact = () => {
-    const form = useRef()
+    const form = useRef(null)
+    const [resultMessage, setResultMessage] = useState("")
 
     const sendEmail = (e) => {
         e.preventDefault()
 
-        console.log(form)
+        setResultMessage("")
+        const params = {name: form.current.name.value, email: form.current.email.value, message: form.current.message.value}
+
+        if(params.name.length == 0 || params.email.length == 0 || params.message.length == 0) {
+            setResultMessage("Field should not be empty")
+        } else {
+            emailjs
+            .send(
+                process.env.REACT_APP_EMAILJS_SERVICE_ID,
+                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+                params,
+                process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+            )
+            .then(
+                (result) => {
+                    //console.log(result.text)
+                    setResultMessage("Message sent")
+                },
+                (error) => {
+                    console.log(error.text)
+                    setResultMessage("Message not sent. Error: " + error.text)
+                }
+            )
+    
+            e.target.reset()
+        }
+        
+        
     }
 
     return (
@@ -37,12 +66,13 @@ const Contact = () => {
                     </div>
                 </div>
                 <div>
-                    <form ref={form} className="form-container" onClick={sendEmail}>
-                        <input className="outlined" type="text" name="user_name" placeholder="Your Name"/>
-                        <input className="outlined" type="email" name="email" placeholder="Your Email"/>
-                        <textarea className="outlined" name="message" placeholder="Your Message" rows="5"/>
+                    <form ref={form} className="form-container" onSubmit={sendEmail}>
+                        <input className="outlined" type="text" name="name" placeholder="Your Name" required/>
+                        <input className="outlined" type="email" name="email" placeholder="Your Email" required/>
+                        <textarea className="outlined" name="message" placeholder="Your Message" rows="5" required/>
                         <input className="btn btn-primary" type="submit" value="Send Message"/>
                     </form>
+                    <span><p style={{textAlign: "left"}}>{resultMessage}</p></span>
                 </div>
             </div>
             
